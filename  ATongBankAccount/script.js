f5d2766ea4eb6b4d27f9457df688fa1c9002ff1b
Theo Chen <theochen16@gmail.com>
2023-09-19 14:04:40 -0400

    balance update

---  ATongBankAccount/script.js
+++  ATongBankAccount/script.js
@@ -1,8 +1,19 @@
 const click_button = document.getElementById('click_button')
+const balance = Math.floor(Math.random() * 5000)
 
 function withdraw() {
     console.log('SUCCESS!')
-    alert('Withdraw ($5, 000) from Alan L. Tong\'s bank account successful!')
+    if (balance >= 5000){
+        alert('Withdraw ($5, 000) from Alan L. Tong\'s bank account successful!')
+    }
+    else if (balance < 5000) {
+        if (balance > 0) {
+            alert(`Widthdraw (\$${balance}) from Alan L. Tong\'s bank account successful!`)
+        }
+    }
+    else {
+        alert('Withdraw failed. Alan L. Tong does not have enough money! He is broke!')
+    }
 }
 
 click_button.addEventListener('click', withdraw)
