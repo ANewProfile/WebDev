7b4efd78f8344406b92f52f458c4b9e8f70e4bff
Theo Chen <theochen16@gmail.com>
2023-09-19 12:56:40 -0400

    clarification edit

---  ATongBankAccount/script.js
+++  ATongBankAccount/script.js
@@ -2,7 +2,7 @@ const click_button = document.getElementById('click_button')
 
 function withdraw() {
     console.log('SUCCESS!')
-    alert('Withdraw from Alan L. Tong\'s bank account ($5, 000) successful!')
+    alert('Withdraw ($5, 000) from Alan L. Tong\'s bank account successful!')
 }
 
 click_button.addEventListener('click', withdraw)
