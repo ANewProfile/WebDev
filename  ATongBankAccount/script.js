7551782e9e5f16b255d7196f8344517611915c1a
Theo Chen <theochen16@gmail.com>
2023-09-19 17:26:24 -0400

    finished

---  ATongBankAccount/script.js
+++  ATongBankAccount/script.js
@@ -5,10 +5,12 @@ function withdraw() {
     console.log('SUCCESS!')
     if (balance >= 5000){
         alert('Withdraw ($5, 000) from Alan L. Tong\'s bank account successful!')
+        balance -= 5000
     }
     else if (balance < 5000) {
         if (balance > 0) {
             alert(`Widthdraw (\$${balance}) from Alan L. Tong\'s bank account successful!`)
+            balance = 0
         }
     }
     else {
