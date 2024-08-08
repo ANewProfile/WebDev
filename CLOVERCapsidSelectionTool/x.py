2cb8ff4f52535790ec5fd043974ec4bced2f8985
Benjie Chen <benjie@ginkgobioworks.com>
2024-08-08 06:51:42 -0400

    fixed typo

--- CLOVERCapsidSelectionTool/x.py
+++
@@ -1,7 +0,0 @@
-def create_fib(num_remain, cur_num, last_num):
-	if num_remain == 0:
-		return cur_num + last_num
-	else:
-		return create_fib(num_remain-1, cur_num+last_num, cur_num)
-
-print(create_fib(98, 1, 1))
